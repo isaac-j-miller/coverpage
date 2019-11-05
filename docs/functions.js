c8297ffee3b41a08d8e8948dfbcb555b8baa6005
isaac-j-miller <44651730+isaac-j-miller@users.noreply.github.com>
2019-11-05 10:55:34 -0500

    style
    fixed smooth scroll for safari (i think)
    added delay and fade in for typing effect

--- docs/functions.js
+++ docs/functions.js
@@ -1,4 +1,3 @@
-
 const slides = 
 [
     {
@@ -15,6 +14,32 @@ const slides =
     }
 ]
 var slidesIndex = -1;
+
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+  
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
+
 function plusSlides(increment){
     slidesIndex +=increment;
     if(slidesIndex < 0){
